5d23a54114abe21a5a70e4430e296386f2443d97
Eber <www.eberacunaquispe@gmail.com>
2026-04-02 23:22:53 -0500

    Archivo de configuracion de EsLint

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      eqeqeq: 'error',
+      'no-unused-vars': 'error',
+      'no-console': 'warn',
+    },
+  },
+  eslintConfigPrettier,
+]);
