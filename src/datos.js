2f3816e32636c80ff93111a91626de7f367dff7f
Eber <www.eberacunaquispe@gmail.com>
2026-04-05 01:34:28 -0500

    Se cambio las metricas y el lenguaje en la url de la api

--- src/datos.js
+++ src/datos.js
@@ -1,7 +1,7 @@
 export async function clima(ubicacion) {
     try {
         const response = await fetch(
-            `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${ubicacion}?key=LUBD9NN97Y74NVVS3FUUXN56S`,
+            `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${ubicacion}?unitGroup=metric&lang=es&key=LUBD9NN97Y74NVVS3FUUXN56S`,
         );
         const clima = await response.json();
         return clima;
