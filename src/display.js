00a5c5dcf5000b131a6c54f766142e5fd32982fe
Eber <www.eberacunaquispe@gmail.com>
2026-04-04 19:47:22 -0500

    Se creo lso archivos del contenedor app que alijara los datos del clima

--- src/display.js
+++ src/display.js
@@ -1,1 +1,115 @@
-export function loadContainer() {}
+import iconoprincipal from './images/iconoapp.png';
+
+export async function cargarIcono(nombre) {
+    const modulo = await import(`./images/${nombre}.svg`);
+    return modulo.default;
+}
+
+export function loadContainer() {
+    seccion1();
+    seccion2();
+    seccion3();
+}
+
+async function seccion1() {
+    const encabezado = document.querySelector('.encabezado');
+    const containertitulo = document.createElement('div');
+    containertitulo.classList.add('containertitulo');
+    const iconotitulo = document.createElement('img');
+    iconotitulo.src = iconoprincipal;
+    const titulo = document.createElement('h1');
+    titulo.textContent = 'CLIMA';
+    const searchcontainer = document.createElement('div');
+    searchcontainer.classList.add('searchcontainer');
+    const input = document.createElement('input');
+    const buscar = document.createElement('button');
+    const buscarimagen = document.createElement('img');
+    buscarimagen.src = await cargarIcono('search');
+
+    encabezado.appendChild(containertitulo);
+    containertitulo.appendChild(iconotitulo);
+    containertitulo.appendChild(titulo);
+    encabezado.appendChild(searchcontainer);
+    searchcontainer.appendChild(input);
+    searchcontainer.appendChild(buscar);
+    buscar.appendChild(buscarimagen);
+
+    return encabezado;
+}
+
+async function seccion2() {
+    const app = document.querySelector('.app');
+
+    const parte1 = climaparte1();
+    const parte2 = climaparte2();
+    const parte3 = climaparte3();
+
+    app.appendChild(parte1);
+    app.appendChild(parte2);
+    app.appendChild(parte3);
+}
+
+function climaparte1() {
+    const parte1 = document.createElement('div');
+    parte1.classList.add('parte1');
+    const titular1 = document.createElement('h1');
+    const datogeneral = document.createElement('div');
+    datogeneral.classList.add('datogeneral');
+    const datosclima = document.createElement('div');
+    datosclima.classList.add('datosclima');
+    const grados = document.createElement('h1');
+    const estado = document.createElement('h2');
+    const sensacion = document.createElement('p');
+    const humedad = document.createElement('p');
+    const viento = document.createElement('p');
+    const indice = document.createElement('p');
+    const imagenclima = document.createElement('img');
+
+    parte1.appendChild(titular1);
+    parte1.appendChild(datogeneral);
+    datogeneral.appendChild(datosclima);
+    datosclima.appendChild(grados);
+    datosclima.appendChild(estado);
+    datosclima.appendChild(sensacion);
+    datosclima.appendChild(humedad);
+    datosclima.appendChild(viento);
+    datosclima.appendChild(indice);
+    datogeneral.appendChild(imagenclima);
+
+    return parte1;
+}
+
+function climaparte2() {
+    const parte2 = document.createElement('div');
+    parte2.classList.add('parte2');
+    const titular2 = document.createElement('h2');
+    const dias = document.createElement('div');
+    dias.classList.add('dias');
+
+    parte2.appendChild(titular2);
+    parte2.appendChild(dias);
+
+    return parte2;
+}
+
+function climaparte3() {
+    const parte3 = document.createElement('div');
+    parte3.classList.add('parte3');
+    const titular3 = document.createElement('h2');
+    const horas = document.createElement('div');
+    horas.classList.add('horas');
+
+    parte3.appendChild(titular3);
+    parte3.appendChild(horas);
+
+    return parte3;
+}
+
+function seccion3() {
+    const pie = document.querySelector('.pie');
+    const piepagina = document.createElement('p');
+    piepagina.textContent =
+        'Datos por Visual Crossing Weather API | Desarrollado por Eber Acuña Quispe';
+
+    pie.appendChild(piepagina);
+}
