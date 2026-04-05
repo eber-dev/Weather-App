43379fb2d460771d1a6649a257c9d08a9c47487e
Eber <www.eberacunaquispe@gmail.com>
2026-04-04 19:49:31 -0500

    Se creo la funcion clima que obtiene los valores climaticos por ubicacion usando fetch y await

--- src/datos.js
+++ src/datos.js
@@ -4,8 +4,8 @@ export async function clima(ubicacion) {
             `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${ubicacion}?key=LUBD9NN97Y74NVVS3FUUXN56S`,
         );
         const clima = await response.json();
-        console.log(clima);
+        return clima;
     } catch (error) {
-        console.log(error);
+        alert(error);
     }
 }
