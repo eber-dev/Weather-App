11c03a200c4583d8e24f36c04ad5e30ff19d3790
Eber <www.eberacunaquispe@gmail.com>
2026-04-06 00:05:03 -0500

    Se completo la funcion de llenar diario

--- src/controller.js
+++ src/controller.js
@@ -7,11 +7,15 @@ async function llenargeneral(clima) {
     const lugarcapitalizado = clima.address;
     lugar.textContent = `${lugarcapitalizado.charAt(0).toUpperCase() + lugarcapitalizado.slice(1)}`;
     const grados = document.querySelector('.grados');
-    grados.textContent = `${clima.currentConditions.temp} °C`;
+    let gradosnumerico = Math.round(parseFloat(clima.currentConditions.temp));
+    grados.textContent = `${gradosnumerico} °C`;
     const estado = document.querySelector('.estado');
     estado.textContent = `${clima.currentConditions.conditions}`;
     const sensacion = document.querySelector('.sensacion');
-    sensacion.textContent = `Sensacion Termica: ${clima.currentConditions.feelslike}`;
+    let sensacionnumerico = Math.round(
+        parseFloat(clima.currentConditions.feelslike),
+    );
+    sensacion.textContent = `Sensacion Termica: ${sensacionnumerico}`;
     const humedad = document.querySelector('.humedad');
     humedad.textContent = `Humedad: ${clima.currentConditions.humidity}%`;
     const viento = document.querySelector('.viento');
@@ -98,7 +102,11 @@ async function llenardiario(clima) {
                 let cambioicono = clima.days[0].icon;
                 cambiarimagenes(iconodia, cambioicono);
                 const rangodia = document.createElement('p');
+                let maximo = Math.round(parseFloat(clima.days[0].tempmax));
+                let minimo = Math.round(parseFloat(clima.days[0].tempmin));
+                rangodia.textContent = `${maximo}/${minimo}`;
                 const estadodia = document.createElement('p');
+                estadodia.textContent = clima.days[0].conditions;
 
                 elemento.appendChild(dia);
                 elemento.appendChild(iconodia);
@@ -116,7 +124,11 @@ async function llenardiario(clima) {
                 let cambioicono = clima.days[1].icon;
                 cambiarimagenes(iconodia, cambioicono);
                 const rangodia = document.createElement('p');
+                let maximo = Math.round(parseFloat(clima.days[1].tempmax));
+                let minimo = Math.round(parseFloat(clima.days[1].tempmin));
+                rangodia.textContent = `${maximo}/${minimo}`;
                 const estadodia = document.createElement('p');
+                estadodia.textContent = clima.days[1].conditions;
 
                 elemento.appendChild(dia);
                 elemento.appendChild(iconodia);
@@ -133,7 +145,11 @@ async function llenardiario(clima) {
                 let cambioicono = clima.days[2].icon;
                 cambiarimagenes(iconodia, cambioicono);
                 const rangodia = document.createElement('p');
+                let maximo = Math.round(parseFloat(clima.days[2].tempmax));
+                let minimo = Math.round(parseFloat(clima.days[2].tempmin));
+                rangodia.textContent = `${maximo}/${minimo}`;
                 const estadodia = document.createElement('p');
+                estadodia.textContent = clima.days[2].conditions;
 
                 elemento.appendChild(dia);
                 elemento.appendChild(iconodia);
@@ -150,7 +166,11 @@ async function llenardiario(clima) {
                 let cambioicono = clima.days[3].icon;
                 cambiarimagenes(iconodia, cambioicono);
                 const rangodia = document.createElement('p');
+                let maximo = Math.round(parseFloat(clima.days[3].tempmax));
+                let minimo = Math.round(parseFloat(clima.days[3].tempmin));
+                rangodia.textContent = `${maximo}/${minimo}`;
                 const estadodia = document.createElement('p');
+                estadodia.textContent = clima.days[3].conditions;
 
                 elemento.appendChild(dia);
                 elemento.appendChild(iconodia);
@@ -167,7 +187,11 @@ async function llenardiario(clima) {
                 let cambioicono = clima.days[4].icon;
                 cambiarimagenes(iconodia, cambioicono);
                 const rangodia = document.createElement('p');
+                let maximo = Math.round(parseFloat(clima.days[4].tempmax));
+                let minimo = Math.round(parseFloat(clima.days[4].tempmin));
+                rangodia.textContent = `${maximo}/${minimo}`;
                 const estadodia = document.createElement('p');
+                estadodia.textContent = clima.days[4].conditions;
 
                 elemento.appendChild(dia);
                 elemento.appendChild(iconodia);
@@ -184,7 +208,11 @@ async function llenardiario(clima) {
                 let cambioicono = clima.days[5].icon;
                 cambiarimagenes(iconodia, cambioicono);
                 const rangodia = document.createElement('p');
+                let maximo = Math.round(parseFloat(clima.days[5].tempmax));
+                let minimo = Math.round(parseFloat(clima.days[5].tempmin));
+                rangodia.textContent = `${maximo}/${minimo}`;
                 const estadodia = document.createElement('p');
+                estadodia.textContent = clima.days[5].conditions;
 
                 elemento.appendChild(dia);
                 elemento.appendChild(iconodia);
@@ -201,7 +229,11 @@ async function llenardiario(clima) {
                 let cambioicono = clima.days[6].icon;
                 cambiarimagenes(iconodia, cambioicono);
                 const rangodia = document.createElement('p');
+                let maximo = Math.round(parseFloat(clima.days[6].tempmax));
+                let minimo = Math.round(parseFloat(clima.days[6].tempmin));
+                rangodia.textContent = `${maximo}/${minimo}`;
                 const estadodia = document.createElement('p');
+                estadodia.textContent = clima.days[6].conditions;
 
                 elemento.appendChild(dia);
                 elemento.appendChild(iconodia);
@@ -212,5 +244,3 @@ async function llenardiario(clima) {
         }
     });
 }
-
-async function llenarhoras(clima) {}
