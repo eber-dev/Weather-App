1cebd3871f18d68cebdbe17915f3538996cb0c76
Eber <www.eberacunaquispe@gmail.com>
2026-04-06 16:09:42 -0500

    Se llama a las funciones utilizando await para clima al ser asincrona y async en la funcion flecha

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './styles.css';
+import { loadContainer } from './display.js';
+import { clima } from './datos.js';
+import { llenardiario, llenargeneral } from './controller.js';
+
+await loadContainer();
+
+const busqueda = document.querySelector('.busqueda');
+const entrada = document.getElementById('entrada');
+
+busqueda.addEventListener('click', async () => {
+    const consulta = entrada.value;
+
+    const climatotal = await clima(consulta);
+    llenargeneral(climatotal);
+    llenardiario(climatotal);
+});
