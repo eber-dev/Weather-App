5815ed231fd4fa076069d8c6cee3039a0e9d7fbd
Eber <www.eberacunaquispe@gmail.com>
2026-04-05 19:28:31 -0500

    Se añadio la funcion de cambiar imagenes para el cambio de los iconos respecto al clima y tambien se creo la funcion crear diario la cual añadira los contenidos de las tarjeta de los clims por dias semanales

--- src/controller.js
+++ src/controller.js
@@ -33,41 +33,184 @@ async function llenargeneral(clima) {
     const imagenclima = document.querySelector('.imagenclima');
     let cambioimages = imagenclima.currentConditions.icon;
 
-    switch (cambioimages) {
+    cambiarimagenes(imagenclima, cambioimages);
+}
+
+export async function cambiarimagenes(imagen, valor) {
+    switch (valor) {
         case 'clear-day':
-            imagenclima.src = await cargarIcono('sunny');
+            imagen.src = await cargarIcono('sunny');
             break;
 
         case 'clear-night':
-            imagenclima.src = await cargarIcono('nightlight');
+            imagen.src = await cargarIcono('nightlight');
             break;
 
         case 'partly-cloudy-day':
-            imagenclima.src = await cargarIcono('partly_cloudy_day');
+            imagen.src = await cargarIcono('partly_cloudy_day');
             break;
 
         case 'partly-cloudy-night':
-            imagenclima.src = await cargarIcono('wb_twilight');
+            imagen.src = await cargarIcono('wb_twilight');
             break;
 
         case 'cloudy':
-            imagenclima.src = await cargarIcono('cloud');
+            imagen.src = await cargarIcono('cloud');
             break;
 
         case 'rain':
-            imagenclima.src = await cargarIcono('rainy');
+            imagen.src = await cargarIcono('rainy');
             break;
 
         case 'snow':
-            imagenclima.src = await cargarIcono('ac_unit');
+            imagen.src = await cargarIcono('ac_unit');
             break;
 
         case 'wind':
-            imagenclima.src = await cargarIcono('air');
+            imagen.src = await cargarIcono('air');
             break;
 
         case 'fog':
-            imagenclima.src = await cargarIcono('foggy');
+            imagen.src = await cargarIcono('foggy');
             break;
     }
 }
+
+async function llenardiario(clima) {
+    const totaldias = document.querySelectorAll('.dia');
+    const nombresdias = [
+        'Domingo',
+        'Lunes',
+        'Martes',
+        'Miércoles',
+        'Jueves',
+        'Viernes',
+        'Sábado',
+    ];
+
+    totaldias.forEach((elemento) => {
+        switch (elemento.dataset.dia) {
+            case 'Dia1': {
+                const dia = document.createElement('h2');
+                const fecha = new Date(clima.days[0].datetime);
+                dia.textContent = `${nombresdias[fecha.getDay()]}`;
+                const iconodia = document.createElement('img');
+                let cambioicono = clima.days[0].icon;
+                cambiarimagenes(iconodia, cambioicono);
+                const rangodia = document.createElement('p');
+                const estadodia = document.createElement('p');
+
+                elemento.appendChild(dia);
+                elemento.appendChild(iconodia);
+                elemento.appendChild(rangodia);
+                elemento.appendChild(estadodia);
+
+                break;
+            }
+
+            case 'Dia2': {
+                const dia = document.createElement('h2');
+                const fecha = new Date(clima.days[1].datetime);
+                dia.textContent = `${nombresdias[fecha.getDay()]}`;
+                const iconodia = document.createElement('img');
+                let cambioicono = clima.days[1].icon;
+                cambiarimagenes(iconodia, cambioicono);
+                const rangodia = document.createElement('p');
+                const estadodia = document.createElement('p');
+
+                elemento.appendChild(dia);
+                elemento.appendChild(iconodia);
+                elemento.appendChild(rangodia);
+                elemento.appendChild(estadodia);
+                break;
+            }
+
+            case 'Dia3': {
+                const dia = document.createElement('h2');
+                const fecha = new Date(clima.days[2].datetime);
+                dia.textContent = `${nombresdias[fecha.getDay()]}`;
+                const iconodia = document.createElement('img');
+                let cambioicono = clima.days[2].icon;
+                cambiarimagenes(iconodia, cambioicono);
+                const rangodia = document.createElement('p');
+                const estadodia = document.createElement('p');
+
+                elemento.appendChild(dia);
+                elemento.appendChild(iconodia);
+                elemento.appendChild(rangodia);
+                elemento.appendChild(estadodia);
+                break;
+            }
+
+            case 'Dia4': {
+                const dia = document.createElement('h2');
+                const fecha = new Date(clima.days[3].datetime);
+                dia.textContent = `${nombresdias[fecha.getDay()]}`;
+                const iconodia = document.createElement('img');
+                let cambioicono = clima.days[3].icon;
+                cambiarimagenes(iconodia, cambioicono);
+                const rangodia = document.createElement('p');
+                const estadodia = document.createElement('p');
+
+                elemento.appendChild(dia);
+                elemento.appendChild(iconodia);
+                elemento.appendChild(rangodia);
+                elemento.appendChild(estadodia);
+                break;
+            }
+
+            case 'Dia5': {
+                const dia = document.createElement('h2');
+                const fecha = new Date(clima.days[4].datetime);
+                dia.textContent = `${nombresdias[fecha.getDay()]}`;
+                const iconodia = document.createElement('img');
+                let cambioicono = clima.days[4].icon;
+                cambiarimagenes(iconodia, cambioicono);
+                const rangodia = document.createElement('p');
+                const estadodia = document.createElement('p');
+
+                elemento.appendChild(dia);
+                elemento.appendChild(iconodia);
+                elemento.appendChild(rangodia);
+                elemento.appendChild(estadodia);
+                break;
+            }
+
+            case 'Dia6': {
+                const dia = document.createElement('h2');
+                const fecha = new Date(clima.days[5].datetime);
+                dia.textContent = `${nombresdias[fecha.getDay()]}`;
+                const iconodia = document.createElement('img');
+                let cambioicono = clima.days[5].icon;
+                cambiarimagenes(iconodia, cambioicono);
+                const rangodia = document.createElement('p');
+                const estadodia = document.createElement('p');
+
+                elemento.appendChild(dia);
+                elemento.appendChild(iconodia);
+                elemento.appendChild(rangodia);
+                elemento.appendChild(estadodia);
+                break;
+            }
+
+            case 'Dia7': {
+                const dia = document.createElement('h2');
+                const fecha = new Date(clima.days[6].datetime);
+                dia.textContent = `${nombresdias[fecha.getDay()]}`;
+                const iconodia = document.createElement('img');
+                let cambioicono = clima.days[6].icon;
+                cambiarimagenes(iconodia, cambioicono);
+                const rangodia = document.createElement('p');
+                const estadodia = document.createElement('p');
+
+                elemento.appendChild(dia);
+                elemento.appendChild(iconodia);
+                elemento.appendChild(rangodia);
+                elemento.appendChild(estadodia);
+                break;
+            }
+        }
+    });
+}
+
+async function llenarhoras(clima) {}
