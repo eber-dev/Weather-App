f1f2c17f9f06b9fcdf18700a4cf03aae39402acb
Eber <www.eberacunaquispe@gmail.com>
2026-04-08 02:32:46 -0500

    Se valida el error al enviar un campo vacio y tambien se asigna el texto de pronostico diario luego de realizar la consulta

--- src/index.js
+++ src/index.js
@@ -7,11 +7,22 @@ await loadContainer();
 
 const busqueda = document.querySelector('.busqueda');
 const entrada = document.getElementById('entrada');
+const titular2 = document.querySelector('.titular2');
 
 busqueda.addEventListener('click', async () => {
     const consulta = entrada.value;
 
+    if (consulta === '') {
+        alert('Debe ingresar una ciudad');
+        return;
+    }
+
     const climatotal = await clima(consulta);
     llenargeneral(climatotal);
     llenardiario(climatotal);
+
+    titular2.textContent = 'Pronostico Diario';
+    entrada.value = '';
 });
+
+console.log(await clima('lima'));
